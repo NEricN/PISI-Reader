78a51d4af26842ed65d808f49da12e6d6f8d6675
Ed Lee <eddieberklee@gmail.com>
2014-03-09 05:33:14 -0700

    handling upload

--- Rails/PISI-Reader/app/controllers/picture_controller.rb
+++ Rails/PISI-Reader/app/controllers/picture_controller.rb
@@ -20,7 +20,7 @@ class PictureController < ApplicationController
     puts "Photo Start >>>>>>>>>>>>>>>>>>>"
     puts 'public/'+@photo.image.url[1..@photo.image.url.length-1]
     puts @photo.image.url
-    image = RTesseract.new(@photo.image.url)
+    image = RTesseract.new(@photo.image.current_path)
     ocr = image.to_s
     @photo.ocr = ocr
     @photo.save if @photo.valid?
