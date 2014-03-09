152a6ce78f95632cc37e42b038078e9ad9364073
Eric Nguyen <ericnguyen@pioneers.berkeley.edu>
2014-03-08 22:25:49 -0800

    Fixed sentence.

--- Rails/PISI-Reader/lib/Sentence.rb
+++ Rails/PISI-Reader/lib/Sentence.rb
@@ -1,14 +1,70 @@
 class Sentence
 	def initialize(string)
-		@words = method() #assigns @words to 2
-    end
+		@sentence = string
+		@words = split_sentence()
 
-	def method
-        2
+		@sentence_length_char = @sentence.size
+		@sentence_length_words = words.length
+
+		@irreg_ending = ending_punct()
+		@nonwords = nonwords_count()
+		@said_ratio = said_ratio_count()
+
+		@ending_forms = forms()
+		@average_word_length = average_word_length_find()
+		@longest_word_length = longest_word_find()
+		@shortest_word_length = shortest_word_find()
+
+		@contractions_length = contractions_find()
+		syllables()
 	end
+
+	def split_sentence()
+		@sentence.split(/[[, ] ]/)
+	end
+
+	def syllables()
+		@syllables = []
+		@syllables_sum = 0
+		@syllable_max = 0
+		@syllable_min = 999
+		@words.each do |word|
+			syllables = syllable_count(word)
+			@syllables.push(syllables)
+			@syllables_sum += syllables
+			@syllable_max = syllables > @syllable_max ? syllables : @syllable_max
+			@syllable_min = syllables < @syllable_min ? syllables : @syllable_min
+		end
+		@syllables_avg = @syllables_sum.to_f/@words.size
+	end
+
+	def syllable_count(word)
+		word.downcase!
+		return 1 if word.length <= 3
+		word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+		word.sub!(/^y/, '')
+		word.scan(/[aeiouy]{1,2}/).size
+	end
+
+	def nonwords_count()
+		temp_sentence = @sentence
+		temp_sentence.sub!(/[^()"-':\/\\\\.]|/, '')
+		temp_sentence.sub!(/\.\.\./, '.')
+		temp_sentence.size
+	end
+
+	def ending_punct()
+		@sentence.scan(/[!?]/).size > 0
+	end
+
+	def said_ratio_count()
+		@sentence.scan(/" said/).size.to_f/@sentence.scan(/"[^"]+"/).size
+	end
+
     def length
         @words.length
     end
+
     def forms
         endings = [/.*ed\W*\z/, /.*ing\W*\z/]
         occurences = Array.new endings.length, 0
@@ -21,14 +77,14 @@ class Sentence
         end
         occurences
     end
-    def average_word_length
+    def average_word_length_find
         total = 0
         (@words).each do |word|
             total += word.length
         end
-        total / @words.length
+        total.to_f / @words.length
     end
-    def longest_word
+    def longest_word_find
         longest = 0
         (@words).each do |word|
             if word.length > longest
@@ -37,7 +93,7 @@ class Sentence
         end
         longest
     end
-    def shortest_word
+    def shortest_word_find
         shortest = 1000
         (@words).each do |word|
             if word.length < shortest
@@ -46,7 +102,7 @@ class Sentence
         end
         shortest
     end
-    def contractions
+    def contractions_find
         count = 0
         (@words).each do |word|
             if /.+'.+/.match(word)
