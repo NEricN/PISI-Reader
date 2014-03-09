1393ec8d7096341644d28a87641ef971fd086d80
Eric Nguyen <ericnguyen@pioneers.berkeley.edu>
2014-03-09 08:09:30 -0700

    Integrated text reading

--- Rails/PISI-Reader/app/controllers/text_controller.rb
+++ Rails/PISI-Reader/app/controllers/text_controller.rb
@@ -1,8 +1,8 @@
 class TextController < ApplicationController
 
 	def upload
-		text = params[:uploaded_text]
+		text = params[:uploaded_text].exists? params[:uploaded_text] : "This is a cow. This is a cat."
 
-		render :text => "B"
+		render :text => grade_text(text)
 	end
 end
