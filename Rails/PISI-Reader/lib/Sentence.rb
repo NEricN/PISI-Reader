f3de463751a90f6faa105726be1eda44809ff3e7
jerryuejio <jerryuejio@gmail.com>
2014-03-09 00:38:09 -0800

    Debug

--- Rails/PISI-Reader/lib/Sentence.rb
+++ Rails/PISI-Reader/lib/Sentence.rb
@@ -2,7 +2,7 @@ class Sentence
 	def initialize(string)
 		@sentence = string
 		@words = split_sentence()
-
+        
 		@sentence_length_char = @sentence.size
 		@sentence_length_words = @words.length
 
@@ -31,7 +31,7 @@ class Sentence
 	end
 
 	def split_sentence()
-		@sentence.split(/\b+/)
+		@sentence.split(/ |, /)
 	end
 
 	def syllables()
@@ -50,15 +50,16 @@ class Sentence
 	end
 
 	def syllable_count(word)
-		word.downcase!
+		word = String.new(word)
+        word.downcase!
 		return 1 if word.length <= 3
 		word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
 		word.sub!(/^y/, '')
 		word.scan(/[aeiouy]{1,2}/).size
 	end
 
-	def nonwords_count()
-		temp_sentence = @sentence
+	def nonwords_count() #Bugs
+		temp_sentence = String.new(@sentence)
 		temp_sentence.sub!(/[^()"-':\/\\\\.]|/, '')
 		temp_sentence.sub!(/\.\.\./, '.')
 		temp_sentence.size
@@ -68,7 +69,7 @@ class Sentence
 		@sentence.scan(/[!?]/).size > 0
 	end
 
-	def said_ratio_count()
+	def said_ratio_count() #Bugs
 		@sentence.scan(/" said/).size.to_f/@sentence.scan(/"[^"]+"/).size
 	end
 
