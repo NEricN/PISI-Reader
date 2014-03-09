af442a569bc2c75c022e6b48611f50493c75b82b
Ed Lee <eddieberklee@gmail.com>
2014-03-08 23:37:59 -0800

    handling upload

--- Rails/PISI-Reader/app/models/photo.rb
+++ Rails/PISI-Reader/app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ActiveRecord::Base
   has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :url  => "/assets/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/:id/:style/:basename.:extension"
-  validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :pic, content_type: { content_type: ['image/jpeg', 'image/png', 'image/gif'] }
+
 end
